510a54e15ffed83ab15df09a94de451ce5260bf4
RezaxOnes <rezaxoneslimited1@gmail.com>
2025-10-21 15:04:12 +0700

    fix issues and have some issues will fix

--- script/email.js
+++ script/email.js
@@ -1,6 +1,6 @@
 
   const user = "rezaxonesassist";
-  const domain = "gmail";
+  const domain = "gmail.com";
   const email = user + "@" + domain;
   const container = document.getElementById("My_Email");
-  container.innerHTML = `<a href="mailto:${email}" style="color:#000000;">${email}.com</a>`;
+  container.innerHTML = `<div><h2 class='incenter' style="font-size:20px;">Get In Touch With Our Team!</h2><br /><a href="mailto:${email}" style="color: #dfdbdbff;"><i class="fa-solid fa-envelope"></i>${email}</a></div>`;
