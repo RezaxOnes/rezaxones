ac307909d9d763ee79595b5266425b8f84d9ea9f
rezaxones <rezaxoneslimited1@gmail.com>
2026-01-24 09:19:50 +0700

    fix + add

--- script/signInLogic.js
+++ script/signInLogic.js
@@ -1,30 +1,44 @@
 
+document.getElementById('signInForm').addEventListener('submit', async function(e) {
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-    document.getElementById('signInForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  try {
+    const res = await fetch('https://singapore.api.hollawgostar.uno/sign_in', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
+
+    let data;
+
     try {
-      const res = await fetch('https://singapore.api.hollawgostar.uno/sign_in', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
-      });
-      const data = await res.json();
-      if (res.ok && data.token) {
-        try {
-          localStorage.setItem('token', data.token);
-          alert("Đã đăng nhập thành công");
-        } catch (error) {
-          alert("Đăng nhập không thành công / Lỗi phía server");
-        }
-      } else {
-        alert(data.error || 'Đăng nhập thất bại');
-      }
+      data = await res.json();
+    } catch {
+      const html = await res.text();
       document.open();
-      document.write(data.html);
+      document.write(html);
       document.close();
-    } catch (err) {
-      alert('Lỗi kết nối database: ' + err.message);
+      return;
     }
-  });
+
+    if (res.ok && data.token) {
+      try {
+        localStorage.setItem('token', data.token);
+        alert("Đã đăng nhập thành công");
+      } catch (error) {
+        alert("Đăng nhập không thành công / Lỗi phía server");
+      }
+    } else {
+      alert(data.error || 'Đăng nhập thất bại');
+    }
+
+    document.open();
+    document.write(data.html);
+    document.close();
+
+  } catch (err) {
+    alert('Lỗi kết nối database: ' + err.message);
+  }
+});
